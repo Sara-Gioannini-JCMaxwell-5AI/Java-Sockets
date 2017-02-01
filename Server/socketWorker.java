eaebf2f461cae0bb6b5bda892543e619c5dc4248
Antohi-Gabriel-JCMaxwell-5Ai <gabriel.antohi@jcmaxwell.it>
2017-02-01 09:47:39 +0100

    Aggiunto la possibilita di creare una GroupChat
    Inserito il codice che permette la creazione di una chat

--- Server/socketWorker.java
+++ Server/socketWorker.java
@@ -12,7 +12,7 @@ import java.io.*;
  */
 class SocketWorker implements Runnable {
   private Socket client;
-
+  public String GroupChat="";
     //Constructor: inizializza le variabili
     SocketWorker(Socket client) {
         this.client = client;
@@ -54,4 +54,51 @@ class SocketWorker implements Runnable {
             System.out.println("Errore connessione con client: " + client);
         }
     }
+  public void gestioneCommandi(String line) {
+if(GroupChat.equals("")){
+            if(line.length()>5){
+            if(line.substring(0, 5).equals("/join"))
+            {
+                GroupChat = line.substring(6, line.length());
+                out.println("Joined to "+line.substring(6, line.length())+" successfully");
+                return;
+            }
+            }
+            //Manda lo stesso messaggio appena ricevuto con in aggiunta il "nome" del client
+                out.println("Server-->" + nickname + ">> " + line);
+            //scrivi messaggio ricevuto su terminale
+                System.out.println(nickname + ">> " + line);
+                }else
+                    {
+                        if(line.equals("/quit"))
+                        {
+                            GroupChat="";
+                            return;
+                        }
+                        if(line.substring(0,7).equals("/invite"))
+                        {
+                            String nickname = line.substring(7,line.length());
+                           for(int i=0;i<ServerTestoMultiThreaded.SocketList.size();i++)
+                            {
+                                if(!ServerTestoMultiThreaded.SocketList.get(i).nickname.equals(nickname) && ServerTestoMultiThreaded.SocketList.get(i).GroupChat.equals(GroupChat) && ServerTestoMultiThreaded.SocketList.get(i).GroupChat.equals(""))
+                                {
+                                    ServerTestoMultiThreaded.SocketList.get(i).GroupChat = GroupChat;
+                                    ServerTestoMultiThreaded.SocketList.get(i).sendMessage("You joined "+GroupChat+" chat");
+                                }
+                                else
+                                {
+                                    sendMessage(nickname+" can't join in this groupchat");
+                                }
+                            }
+                        }
+                        for(int i=0;i<ServerTestoMultiThreaded.SocketList.size();i++)
+                            {
+                                if(!ServerTestoMultiThreaded.SocketList.get(i).nickname.equals(nickname) && ServerTestoMultiThreaded.SocketList.get(i).GroupChat.equals(GroupChat))
+                                {
+                                    ServerTestoMultiThreaded.SocketList.get(i).sendMessage(nickname+">"+line);
+                                }
+                            }
+                        
+                    }
+                }
 }
